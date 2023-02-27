19f0282c6e1b781f35f6770868337cd002de40eb
Bee Nguyen <thangnh.info@gmail.com>
2023-02-27 17:40:26 +0700

    get picture from base url

--- crawling_data.py
+++ crawling_data.py
@@ -1,12 +1,14 @@
-# import pandas as pd
 import random
 import time
+import urllib
+
 import pymongo
 from bs4 import BeautifulSoup
 import requests
-from urllib.request import urlopen
 import json
 import re
+import os
+
 from config import host, db_name, collection
 
 headers = {"user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36"}
@@ -32,11 +34,6 @@ for p in categories:
         for item in json.loads(result).get('data'):
             id_product.append(item.get('id'))
         time.sleep(random.randrange(1, 2))
-        # for n_item in range (len(soup.find_all('a', class_='product-item'))):
-        #     print('Product {}'.format(n_item))
-        #     id = soup.find_all('a', class_='product-item')[n_item].attrs['data-view-content']
-        #     e = json.loads(id)
-        #     id_product.append(e.get('click_data').get('id'))
         num += 1
 
 myclient = pymongo.MongoClient(host)
@@ -47,15 +44,21 @@ data = []
 data_err = []
 
 for img in id_product:
-    print('Add {} to db'.format(img))
+    print('Process {}'.format(img))
     api = 'https://tiki.vn/api/v2/products/{}'.format(img)
     try:
-        res = urlopen(api)
+        res = urllib.urlopen(api)
     except:
         continue
     soup = BeautifulSoup(res.read(), 'html.parser')
     data.append(json.loads(soup.text))
     mycol.insert_one(json.loads(soup.text))
+    n_image = 0
+    os.makedirs(os.path.join('images', str(img)))
+    for n in range(len(json.loads(soup.text).get('images'))):
+        urllib.request.urlretrieve(json.loads(soup.text).get('images')[n_image].get('base_url'),
+                                   os.path.join('images', str(img), str(img) + '_' + str(n_image) + '.jpg'))
+        n_image += 1
     time.sleep(random.randrange(0, 3))
 
 jsonString = json.dumps(data, ensure_ascii=False)
@@ -63,42 +66,3 @@ with open("data.json", "w", encoding='utf-8') as outfile:
     outfile.write(jsonString)
 
 mycol.create_index('short_description')
-
-# https://tiki.vn/do-choi-me-be/c2549
-# https://tiki.vn/tikingon/ngon/c44792
-# https://tiki.vn/dien-thoai-may-tinh-bang/c1789
-# https://tiki.vn/lam-dep-suc-khoe/c1520
-# https://tiki.vn/dien-gia-dung/c1882
-# https://tiki.vn/thoi-trang-nu/c931
-# https://tiki.vn/thoi-trang-nam/c915
-# https://tiki.vn/giay-dep-nu/c1703
-# https://tiki.vn/tui-vi-nu/c976
-# https://tiki.vn/giay-dep-nam/c1686
-# https://tiki.vn/tui-thoi-trang-nam/c27616
-# https://tiki.vn/balo-va-vali/c6000
-# https://tiki.vn/phu-kien-thoi-trang/c27498
-# https://tiki.vn/dong-ho-va-trang-suc/c8371
-# https://tiki.vn/laptop-may-vi-tinh-linh-kien/c1846
-# https://tiki.vn/nha-cua-doi-song/c1883
-# https://tiki.vn/cross-border-hang-quoc-te/c17166
-# https://tiki.vn/bach-hoa-online/c4384
-# https://tiki.vn/thiet-bi-kts-phu-kien-so/c1815
-# https://tiki.vn/voucher-dich-vu/c11312
-# https://tiki.vn/o-to-xe-may-xe-dap/c8594
-# https://tiki.vn/nha-sach-tiki/c8322
-# https://tiki.vn/dien-tu-dien-lanh/c4221
-# https://tiki.vn/the-thao-da-ngoai/c1975
-# https://tiki.vn/may-anh/c1801
-# https://tiki.vn/san-pham-tai-chinh-bao-hiem/c54042
-
-# id = soup.find_all('a', class_='product-item')[0].attrs['data-view-content']
-# e = json.loads(id)
-# e.get('click_data').get('id')
-
-# with open("data.json", "w", encoding='utf-8') as outfile:
-#     outfile.write(jsonString)
-
-# f = open("data.json", "r", encoding='utf-8')
-# print(f.read())
-
-# jsonString = json.dumps(data_err, ensure_ascii=False)
